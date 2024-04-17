863e4810537cf4ec12ee141aa10204c25155339d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-04-18 06:41:29 +0700

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,108 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+
+      <div>
+        <label htmlFor="midterm">คะแนนสอบกลางภาค (เต็ม 40%)</label>
+        <input type="number" name="midterm" />
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <label htmlFor="midterm">คะแนนเก็บ  (เต็ม 15%)</label>
+        <input type="number" name="midterm" />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
+}
+
+
+function calcGrade(
+  midterm: number,
+  attentdance: number,
+  quiz: number,
+  assignment: number
+) {
+  const gradeCeiling = {
+    A: 71.79,
+    "B+": 66.5,
+    B: 61.21,
+    "C+": 55.92,
+    C: 50.63,
+    "D+": 45.34,
+    D: 40.05,
+  }
+
+  let finalScoreNoF = 45 - (midterm + attentdance + quiz + assignment)
+  let finalPercentNoF = (finalScoreNoF / 45) * 100
+  finalScoreNoF = rounded(finalScoreNoF)
+  finalPercentNoF = rounded(finalPercentNoF)
+
+  if (finalScoreNoF < 0) {
+    finalScoreNoF = 0
+    finalPercentNoF = 0
+  }
+
+  const mean_current = 51.774912
+  const mean_target = 55.92
+  const std_current = 10.370103
+  const std_target = 10.58
+
+  const totalScore = (midterm / 40) * 85 + attentdance + quiz + assignment
+  const totalScoreAdjusted =
+    mean_target + ((totalScore - mean_current) * std_target) / std_current
+
+  let gradeLetter = ""
+  if (totalScoreAdjusted > gradeCeiling["A"]) {
+    gradeLetter = "A"
+  } else if (totalScoreAdjusted > gradeCeiling["B+"]) {
+    gradeLetter = "B+"
+  } else if (totalScoreAdjusted > gradeCeiling["B"]) {
+    gradeLetter = "B"
+  } else if (totalScoreAdjusted > gradeCeiling["C+"]) {
+    gradeLetter = "C+"
+  } else if (totalScoreAdjusted > gradeCeiling["C"]) {
+    gradeLetter = "C"
+  } else if (totalScoreAdjusted > gradeCeiling["D+"]) {
+    gradeLetter = "D+"
+  } else if (totalScoreAdjusted > gradeCeiling["D"]) {
+    gradeLetter = "D"
+  } else {
+    gradeLetter = "F"
+  }
+
+  const gradeLetters = ["A", "B+", "B", "C+", "C", "D+", "D"] as const
+  const finalTargetScores = {} as typeof gradeCeiling
+  const finalTargetPercents = {} as typeof gradeCeiling
+  gradeLetters.forEach((gl) => {
+    const totalScore =
+      ((gradeCeiling[gl] - mean_target) * std_current) / std_target +
+      mean_current
+
+    let finalTargetScore = totalScore - (attentdance + quiz + assignment + midterm)
+    if (finalTargetScore > 45) {
+      finalTargetScore = 45
+    }
+    else if (finalTargetScore < 0) {
+      finalTargetScore = 0
+    }
+    let finalTargetPercent = finalTargetScore / 45 * 100
+
+
+    finalTargetScore = rounded(finalTargetScore)
+    finalTargetPercent = rounded(finalTargetPercent)
+
+    finalTargetScores[gl] = finalTargetScore
+    finalTargetPercents[gl] = finalTargetPercent
+  })
+
+
+  return { gradeLetter, finalScoreNoF, finalPercentNoF, finalTargetScores, finalTargetPercents }
+}
+
+function rounded(n: number) {
+  return Number((Math.round(n * 100) / 100).toFixed(1))
 }
